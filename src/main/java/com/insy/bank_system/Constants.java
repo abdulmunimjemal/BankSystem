24a125e71d04d40209c15c926bb646472fad95d9
Abdulmunim <amunim729@gmail.com>
2023-07-02 14:41:10 +0300

    Initial commit

---
+++ src/main/java/com/insy/bank_system/Constants.java
@@ -0,0 +1,9 @@
+package com.insy.bank_system;
+
+public class Constants {
+    public final static String CONNECTION_URL = "jdbc:mysql://localhost:3306/bank";
+    public final static String DB_USERNAME = "root";
+    public final static String DB_PASSWORD = "munimpro1";
+    public final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+}
---
+++ src/main/java/com/insy/bank_system/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.insy.bank_system;
+
+/**
+ *
+ * @author CTM
+ */
+public class Main {
+    public static void main(String[] args) {
+        new LoginPage().run();
+    }
+}
